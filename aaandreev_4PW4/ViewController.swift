c027b7c05dd798b4131dc2e1fca5c01abe984f51
Andreev Anton <boy.of.future@mail.ru>
2021-10-28 17:35:26 +0300

    1 Point: The project contains NavigationController and CollectionView with cell

--- aaandreev_4PW4/ViewController.swift
+++ aaandreev_4PW4/ViewController.swift
@@ -7,11 +7,28 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    
+
+    @IBOutlet weak var emptyCollectionLabel: UILabel!
+    @IBOutlet weak var notesCollectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(
+            withReuseIdentifier: "NoteCell",
+            for: indexPath
+        )
+        return cell
     }
 
 
