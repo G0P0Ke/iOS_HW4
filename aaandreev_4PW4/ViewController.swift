87ed15c8f7d820a55eae7882a9bca534002bfa15
Andreev Anton <boy.of.future@mail.ru>
2021-10-29 00:19:34 +0300

    5 Points: Pressing “done” button creates new note and it shows on CollectionView

--- aaandreev_4PW4/ViewController.swift
+++ aaandreev_4PW4/ViewController.swift
@@ -7,12 +7,22 @@
 
 import UIKit
 
-class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+struct Note {
+    let title: String
+    let description: String
+}
 
-    
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
     @IBOutlet weak var emptyCollectionLabel: UILabel!
     @IBOutlet weak var notesCollectionView: UICollectionView!
+    
+    var notes: [Note] = [] {
+        didSet {
+            emptyCollectionLabel.isHidden = notes.count != 0
+            notesCollectionView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,25 +32,38 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
             action: #selector(createNote(sender:)))
     }
     
+    
+    
     @objc func createNote(sender: UIBarButtonItem) {
         guard let vc = storyboard?.instantiateViewController(
                withIdentifier: "NoteViewController"
-        ) else {
+        ) as? NoteViewController else {
            return
         }
+        vc.outputVC = self
         navigationController?.pushViewController(vc, animated: true)
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
-        return 1
+        return notes.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfLine: CGFloat = 5
+        let cellWidth = UIScreen.main.bounds.size.width
+        let cellHeight = UIScreen.main.bounds.size.height / numberOfLine
+        return CGSize(width: cellWidth, height: cellHeight)
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(
             withReuseIdentifier: "NoteCell",
             for: indexPath
-        )
+        ) as! NoteCell
+        let note = notes[indexPath.row]
+        cell.titleLabel.text = note.title
+        cell.descriptionLabel.text = note.description
         return cell
     }
 }
